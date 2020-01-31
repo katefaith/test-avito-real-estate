693a4cfbe9f3162b28e4f12cb7082ee632e59ba6
Kate <chelsea-92@mail.ru>
2020-01-31 21:52:41 +0300

    fix slider init

--- js/details.js
+++ js/details.js
@@ -10,6 +10,7 @@ function loadInfo() {
         .then(data => {
             renderDetails(data);
             renderSlides(data);
+            initSlider();
         })
         .catch(error => console.log(error))
 }
@@ -45,4 +46,28 @@ function renderSlides(item) {
         img.setAttribute('alt', `image ${index + 1}`);
         document.querySelector('.slider__container').appendChild(slide);
     });
+}
+
+function initSlider() {
+    let slides = document.querySelectorAll('.slider__item');
+    let btnNext = document.querySelector('.slider__btn-next');
+    let btnPrev = document.querySelector('.slider__btn-prev');
+    let currentSlide = 0;
+
+    btnNext.addEventListener('click', showNextSlide);
+    btnPrev.addEventListener('click', showPrevSlide);
+
+    function showNextSlide() {
+        goToSlide(currentSlide + 1);
+    }
+    
+    function showPrevSlide() {
+        goToSlide(currentSlide - 1);
+    }
+    
+    function goToSlide(n) {
+        slides[currentSlide].classList.remove('slider__item--active');
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].classList.add('slider__item--active');
+    }
 }
